8691fb8e7c0057229de7ef0a251fb19322f774bc
maartz <maartz@protonmail.com>
2020-06-10 11:33:12 +0200

    minor changes routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :schedules
   resources :rounds
-  root to: "home#index"
+  root to: 'home#index'
   get 'home/index'
   resources :stations
   resources :rounds_trackings
-
 end
