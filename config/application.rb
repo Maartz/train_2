11a5e1e450300916fd1172220ff2527ed40381b3
Maartz <maartz@protonmail.com>
2020-08-07 10:26:11 +0200

    Update application.rb
    Comment theme which causes side effects on non git configured machines

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-ENV['RAILS_ADMIN_THEME'] = 'softwarebrothers_theme'
+# ENV['RAILS_ADMIN_THEME'] = 'softwarebrothers_theme'
 
 module Train2
   class Application < Rails::Application
